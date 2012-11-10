1c399e930ef8e15dcfd1cbffdb67303c6139932b
Machir <jasperhidding@gmail.com>
2012-11-10 21:38:12 +0100

    Started working on gui slots.

---
+++ SteamPilots/Gui/GuiSlot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamPilots
+{
+    public class GuiSlot : GuiElement
+    {
+        public GuiSlot()
+        {
+        }
+    }
+}
