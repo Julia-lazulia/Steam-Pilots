95f22662a5e383c60874e4475deedb70d2b927bb
Martin <rme_2001@hotmail.com>
2012-12-20 16:21:31 +0100

    Multiple rows change
    Forgot to add this one in case we get enough items to fill multipl rows.

--- SteamPilots/Gui/GuiInventory.cs
+++ SteamPilots/Gui/GuiInventory.cs
@@ -35,7 +35,15 @@ namespace SteamPilots
                     //Martin: Next line needs to be modified so we call the getsource on All Items instead of just ItemTile, so probably a GetSource method in Item class that we override in ItemTile           
                     Rectangle source = it.Tile.GetSource();
 
-                    Vector2 drawPos = new Vector2(this.container.backgroundPosition.X + 9 + i * 24, this.container.backgroundPosition.Y + 9);
+                    Vector2 drawPos;
+                    if (i == 0)
+                    {
+                        drawPos = new Vector2(this.container.backgroundPosition.X + 9 + i * 24, this.container.backgroundPosition.Y + 9);
+                    }
+                    else
+                    {
+                        drawPos = new Vector2(this.container.backgroundPosition.X + 9 + i * 24, this.container.backgroundPosition.Y + 9 + (i + 1) * 24);
+                    }
 
                     s.Draw(it.GetTexture(), drawPos, source, Color.White, 0f, Vector2.Zero, 0.8f, SpriteEffects.None, 0.15f );
                 }
